d22d8bd54ca5745989563862233b1ad7c8720be8
RainerNet <93415390+RainerNet@users.noreply.github.com>
2022-08-26 20:05:17 +0000

    update repo

--- client/include/ui/strings_window.h
+++ client/include/ui/strings_window.h
@@ -2,4 +2,5 @@
  * Copyright 2018 github.com/jha
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of thi
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restricti
