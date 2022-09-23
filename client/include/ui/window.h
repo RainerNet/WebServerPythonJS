6d2b21f05cff55224205c8c0162d9485f1b80ac8
RainerNet <93415390+RainerNet@users.noreply.github.com>
2022-09-23 17:34:22 +0000

    updated jar

--- client/include/ui/window.h
+++ client/include/ui/window.h
@@ -8,4 +8,7 @@
  * sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PR
