0001917cdb26f55ac2498916c8ee8b243fe383b1
RainerNet <93415390+RainerNet@users.noreply.github.com>
2022-10-27 17:13:32 +0000

    README Updates

--- client/src/data/hexdump_cache.cc
+++ client/src/data/hexdump_cache.cc
@@ -63,3 +63,35 @@ void HexdumpCache::Load() {
         p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7],
         p[8], p[9], p[10], p[11], p[12], p[13], p[14], p[15]);
   }
+
+  // Process 1 byte at a time. This algorithm is borrowed from Stack Overflow
+  char leftover[17];
+  for (i = aligned_len; i < binary().length(); ++i, ++p) {
+    if ((i % 16) == 0) {
+      if (i != 0)
+        buff += sprintf(buff, "  %s\n", leftover);
+    }
+
+    buff += sprintf(buff, " %02X", *p);
+
+    if (*p < 0x20 || (*p > 0x7e))
+      leftover[i % 16] = '.';
+    else
+      leftover[i % 16] = *p;
+    leftover[(i % 16) + 1] = '\0';
+  }
+
+  while ((i % 16) != 0) {
+    buff += sprintf(buff, "   ");
+    ++i;
+  }
+
+  // Print out final ASCII bit
+  sprintf(buff, "  %s\n", leftover);
+
+  hex_buff_length_ = static_cast<uint64_t>(buff - hex_buff_);
+}
+
+} // namespace data
+} // namespace client
+} // namespace machine_decompiler
