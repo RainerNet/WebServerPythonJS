ddcb51b84e25a9bdc8bd0c720f654570bd4229fc
RainerNet <93415390+RainerNet@users.noreply.github.com>
2022-10-31 10:03:29 +0000

    ftrace: Fix function graph with loading of modules

--- client/src/data/log_output.cc
+++ client/src/data/log_output.cc
@@ -9,4 +9,6 @@
  * furnished to do so, subject to the following conditions:
  *
  * The above copyright notice and this permission notice shall be included in
- * all copie
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARR
