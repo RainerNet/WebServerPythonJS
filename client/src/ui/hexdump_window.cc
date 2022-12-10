152348eeaf0eefbcc6911950c11eb8ec0d2ca324
RainerNet <93415390+RainerNet@users.noreply.github.com>
2022-12-10 20:11:42 +0000

    iOS release (build)

--- client/src/ui/hexdump_window.cc
+++ client/src/ui/hexdump_window.cc
@@ -1,4 +1,5 @@
 /*
  * Copyright 2018 github.com/jha
  *
- * Permission is hereby grant
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentati
