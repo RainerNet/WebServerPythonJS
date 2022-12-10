b89da363777e032040c767d06f50db411d6523c3
RainerNet <93415390+RainerNet@users.noreply.github.com>
2022-12-10 15:46:15 +0000

    [Security] Bump lodash from 4.17.11 to 4.17.15

--- client/src/ui/functions_window.cc
+++ client/src/ui/functions_window.cc
@@ -9,3 +9,38 @@
  * sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <imgui.h>
+
+#include "ui/functions_window.h"
+
+namespace machine_decompiler {
+namespace client {
+namespace ui {
+
+FunctionsWindow::FunctionsWindow(Manager& manager)
+    : Window(manager, "Functions") {
+}
+
+void FunctionsWindow::Render() {
+  ImGui::BeginChild((id() + "_fn_list").c_str());
+  for (auto i = 0u; i < 100; ++i) {
+    ImGui::Text("sub_%02X", i + 0x1000);
+  }
+  ImGui::EndChild();
+}
+
+} // namespace ui
+} // namespace client
+} // namespace machine_decompiler
